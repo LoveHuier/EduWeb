4b1e33fee02cda525113242ceee5a7148dda6d91
mata <gaoyp_1@sina.com>
2018-05-21 14:45:37 +0800

    config setttings.py to connection mysql

--- EduWeb/settings.py
+++ EduWeb/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'EduWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edu_schema',
+        'USER': 'root',
+        'PASSWORD': 'ts123456',
+        'HOST': '127.0.0.1',
     }
 }
 
