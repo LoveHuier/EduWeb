d363e9b139ffbf4cb39a4abe9a20b80d51a52a6e
mata <gaoyp_1@sina.com>
2018-05-22 18:17:08 +0800

    modify menu style

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -12,6 +12,13 @@ class BaseSetting(object):
     use_bootswatch = True
 
 
+class GlobalSettings(object):
+    site_title = "后台管理系统"
+    site_footer = "自学网"
+    # 目录折叠
+    menu_style = "accordion"
+
+
 class EmailVerifyRecordAdmin(object):
     # 显示样式
     list_display = ("code", "email", "send_type", "send_time")
@@ -30,3 +37,4 @@ class BannerAdmin(object):
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
 xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
