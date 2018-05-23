3ed62ed7d102ff1b0cdb4a00735550c498fd1c6a
mata <gaoyp_1@sina.com>
2018-05-23 15:00:49 +0800

    Complete login function

--- EduWeb/urls.py
+++ EduWeb/urls.py
@@ -18,10 +18,12 @@ from django.urls import path
 from django.views.generic import TemplateView
 import xadmin
 
+from users.views import user_login
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
     # 调用TemplateView类的as_view方法，会自动转化一个view的函数过来，并在template_name参数指定文件即可，不用自己写一个view类
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("login/", TemplateView.as_view(template_name="login.html"), name="login"),
+    path("login/", user_login, name="login"),
 ]
