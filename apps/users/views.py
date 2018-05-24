5843624f3f1257fbf286760a4cb6ae7c5e125daf
mata <gaoyp_1@sina.com>
2018-05-24 17:37:03 +0800

    Encrypt password

--- apps/users/views.py
+++ apps/users/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 # 引进django中的一个基础的View,完成定制功能
 from django.views.generic.base import View
+# 对密码进行加密
+from django.contrib.auth.hashers import make_password
 
 from .models import UserProfile
 from .forms import LoginForm, RegisterForm
@@ -32,6 +34,16 @@ class RegisterView(View):
     def post(self, request):
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
+            user_name = request.POST.get("email", "")
+            pass_word = request.POST.get("password", "")
+
+            user_profile = UserProfile()
+            user_profile.username = user_name
+            user_profile.email = user_name
+            # 对密码进行加密
+            user_profile.password = make_password(pass_word)
+
+            user_profile.save()
             return render(request, "login.html")
 
 
