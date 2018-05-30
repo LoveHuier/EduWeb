0de4ef5c643c935126baedd7277baa4d260f5ce4
mata <gaoyp_1@sina.com>
2018-05-30 11:00:59 +0800

    add field category in organization's model

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -12,6 +12,9 @@ class CityDict(models.Model):
         verbose_name = u"城市"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class CourseOrg(models.Model):
     city = models.ForeignKey(CityDict, verbose_name=u"所在城市", on_delete=models.CASCADE)
@@ -19,13 +22,18 @@ class CourseOrg(models.Model):
     desc = models.TextField(verbose_name=u"机构描述")
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
-    image = models.ImageField(upload_to="org/%Y/%m", verbose_name=u"封面图", max_length=100)
+    category = models.CharField(max_length=20, verbose_name=u"机构类别", default="pxjg",
+                                choices=(("pxjg", u"培训机构"), ("gr", u"个人"), ("gx", u"高校")))
+    image = models.ImageField(upload_to="org/%Y/%m", verbose_name=u"logo", max_length=100)
     address = models.CharField(max_length=150, verbose_name=u"机构地址")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"课程机构"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
 
 
 class Teacher(models.Model):
