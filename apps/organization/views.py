7019d619448b59fab7517e731829aa1402a7162f
mata <gaoyp_1@sina.com>
2018-05-30 16:20:28 +0800

    complete paging function

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import CourseOrg, CityDict
 
@@ -16,8 +17,19 @@ class OrgView(View):
         all_orgs = CourseOrg.objects.all()
         org_nums = all_orgs.count()
         all_citys = CityDict.objects.all()
+
+        # 对课程机构进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_orgs, 5, request=request)
+
+        orgs = p.page(page)
+
         return render(request, "org-list.html", {
-            "all_orgs": all_orgs,
+            "all_orgs": orgs,
             "all_citys": all_citys,
             "org_nums": org_nums,
         })
